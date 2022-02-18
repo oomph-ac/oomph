ce491a7a5aca761ffa232b87121f3411f65c9497
Hashim <13991048+Prim69@users.noreply.github.com>
2022-02-17 19:16:57 -0500

    add allower and closer

---
+++ oomph/connection.go
@@ -0,0 +1,25 @@
+package oomph
+
+import (
+	"github.com/sandertv/gophertunnel/minecraft"
+	"github.com/sandertv/gophertunnel/minecraft/protocol/login"
+	"net"
+)
+
+// Allower may be implemented to specifically allow or disallow players from joining a Server, by setting the specific
+// Allower implementation through a call to Oomph.Allow.
+type Allower interface {
+	// Allow filters what connections are allowed to connect to Oomph. The address, identity data, and client data
+	// of the connection are passed. If Admit returns false, the connection is closed with the string returned
+	// as the disconnect message. WARNING: Use the client data at your own risk, it cannot be trusted because
+	// it can be freely changed by the player connecting.
+	Allow(addr net.Addr, d login.IdentityData, c login.ClientData) (string, bool)
+}
+
+// Closer may be implemented to handle when a player leaves the server. This is meant for cases where the player leaves
+// before they join, where Dragonfly hasn't created the player yet and you can't use the quit handler.
+type Closer interface {
+	// Close allows you to handle the player leaving the server. This is necessary incase you're using Allower
+	// and the player leaves before they fully join, it allows you to unset anything you may have saved such as a player session.
+	Close(c *minecraft.Conn)
+}
