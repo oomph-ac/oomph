18f0a41643751bac208259e2d5482030ad7ae950
Hashim <13991048+Prim69@users.noreply.github.com>
2022-02-24 20:40:26 -0500

    fix major things

--- check/killaura_a.go
+++ check/killaura_a.go
@@ -5,34 +5,34 @@ import (
 	"github.com/sandertv/gophertunnel/minecraft/protocol/packet"
 )
 
-// KillAuraA checks if a player is attacking an entity without swinging their arm.
-type KillAuraA struct {
+// KillauraA checks if a player is attacking an entity without swinging their arm.
+type KillauraA struct {
 	basic
 	lastSwingTick uint64
 }
 
-// NewKillAuraA creates a new KillAuraA check.
-func NewKillAuraA() *KillAuraA {
-	return &KillAuraA{}
+// NewKillAuraA creates a new KillauraA check.
+func NewKillAuraA() *KillauraA {
+	return &KillauraA{}
 }
 
 // Name ...
-func (*KillAuraA) Name() (string, string) {
+func (*KillauraA) Name() (string, string) {
 	return "KillAura", "A"
 }
 
 // Description ...
-func (*KillAuraA) Description() string {
+func (*KillauraA) Description() string {
 	return "This checks if a player is attacking without swinging their arm."
 }
 
 // MaxViolations ...
-func (*KillAuraA) MaxViolations() float64 {
+func (*KillauraA) MaxViolations() float64 {
 	return 15
 }
 
 // Process ...
-func (k *KillAuraA) Process(processor Processor, pk packet.Packet) {
+func (k *KillauraA) Process(processor Processor, pk packet.Packet) {
 	switch pk := pk.(type) {
 	case *packet.Animate:
 		if pk.ActionType == packet.AnimateActionSwingArm {
