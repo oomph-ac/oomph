4ec5b61f07b0bc39eedbce047f61a63c84ab4d77
JustTal <justtal@protonmail.com>
2022-02-28 00:08:46 -0600

    fuck this shit im out

--- utils/world.go
+++ utils/world.go
@@ -5,7 +5,6 @@ import (
 	"github.com/df-mc/dragonfly/server/block/cube"
 	"github.com/df-mc/dragonfly/server/entity/physics"
 	"github.com/df-mc/dragonfly/server/world"
-	"github.com/go-gl/mathgl/mgl64"
 	"math"
 )
 
@@ -20,7 +19,7 @@ func BlockClimbable(b world.Block) bool {
 }
 
 // BlocksNearby returns a slice of blocks that are nearby the search position.
-func BlocksNearby(pos mgl64.Vec3, aabb physics.AABB, w *world.World) []world.Block {
+func BlocksNearby(aabb physics.AABB, w *world.World) []world.Block {
 	grown := aabb.Grow(1)
 	min, max := grown.Min(), grown.Max()
 	minX, minY, minZ := min[0], min[1], min[2]
@@ -30,7 +29,7 @@ func BlocksNearby(pos mgl64.Vec3, aabb physics.AABB, w *world.World) []world.Blo
 	for y := minY; y < maxY; y++ {
 		for x := minX; x < maxX; x++ {
 			for z := minZ; z < maxZ; z++ {
-				blocks = append(blocks, w.Block(cube.PosFromVec3(pos).Add(cube.Pos{int(x), int(y), int(z)})))
+				blocks = append(blocks, w.Block(cube.Pos{int(x), int(y), int(z)}))
 			}
 		}
 	}
@@ -38,36 +37,24 @@ func BlocksNearby(pos mgl64.Vec3, aabb physics.AABB, w *world.World) []world.Blo
 }
 
 // CollidingBlocks returns all block AABBs that collide with the entity.
-func CollidingBlocks(aabb physics.AABB, pos mgl64.Vec3, w *world.World) []physics.AABB {
+func CollidingBlocks(aabb physics.AABB, w *world.World) []physics.AABB {
 	grown := aabb.Grow(1)
 	min, max := grown.Min(), grown.Max()
 	minX, minY, minZ := int(math.Floor(min[0])), int(math.Floor(min[1])), int(math.Floor(min[2]))
 	maxX, maxY, maxZ := int(math.Floor(max[0])), int(math.Floor(max[1])), int(math.Floor(max[2]))
 
-	blockAABBs := make([]physics.AABB, 0, (maxX-minX)*(maxY-minY)*(maxZ-minZ))
+	var blockAABBs []physics.AABB
 	for y := minY; y <= maxY; y++ {
 		for x := minX; x <= maxX; x++ {
 			for z := minZ; z <= maxZ; z++ {
 				blockPos := cube.Pos{x, y, z}
-				if b := w.Block(blockPos); !PassThroughBlock(aabb, pos, blockPos, w) {
-					for _, bb := range b.Model().AABB(blockPos, w) {
-						if bb.Translate(blockPos.Vec3()).IntersectsWith(aabb) {
-							blockAABBs = append(blockAABBs, bb)
-						}
+				for _, box := range w.Block(blockPos).Model().AABB(blockPos, w) {
+					if box.Translate(blockPos.Vec3()).IntersectsWith(aabb) {
+						blockAABBs = append(blockAABBs, box.Translate(blockPos.Vec3()))
 					}
 				}
 			}
 		}
 	}
 	return blockAABBs
-}
-
-// PassThroughBlock returns true if the entity can pass through the block at the given position.
-func PassThroughBlock(aabb physics.AABB, pos mgl64.Vec3, blockPos cube.Pos, w *world.World) bool {
-	for _, bb := range w.Block(blockPos).Model().AABB(blockPos, w) {
-		if bb.Grow(0.05).Translate(blockPos.Vec3()).IntersectsWith(aabb.Translate(pos)) {
-			return true
-		}
-	}
-	return false
 }
