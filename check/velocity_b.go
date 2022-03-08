61e475e5abe40c4efbf049398e8017d5b7df96ba
Cqdet <60723938+Cqdet@users.noreply.github.com>
2022-03-07 22:46:03 -0500

    fix whoever decided to add this random space here

--- check/velocity_b.go
+++ check/velocity_b.go
@@ -37,7 +37,6 @@ func (v *VelocityB) Process(p Processor, pk packet.Packet) {
 	case *packet.PlayerAuthInput:
 		if p.MotionTicks() == 1 && math.Abs(p.PreviousServerPredictedMotion().X()) > 0.01 &&
 			math.Abs(p.PreviousServerPredictedMotion().Z()) > 0.01 {
-
 			xVal := p.Motion().X() / p.PreviousServerPredictedMotion().X()
 			zVal := p.Motion().Z() / p.PreviousServerPredictedMotion().Z()
 			if ((xVal <= 0.9999 && zVal <= 0.9999) || (xVal >= 1.5 || zVal >= 1.5)) && !p.Teleporting() && !p.CollidedHorizontally() {
