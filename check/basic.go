3354d7555c7dbc6113236a3304791c1ab07e7289
JustTal <justtal@protonmail.com>
2022-04-25 16:21:30 -0500

    check/basic.go: Various changes.

--- check/basic.go
+++ check/basic.go
@@ -10,32 +10,32 @@ type basic struct {
 }
 
 // Buff adds to the buffer and returns the new one.
-func (t *basic) Buff(n float64, max ...float64) float64 {
+func (b *basic) Buff(n float64, max ...float64) float64 {
 	m := 15.0
 	if len(max) > 0 {
 		m = max[0]
 	}
-	t.buffer += n
-	t.buffer = math.Max(0, t.buffer)
-	t.buffer = math.Min(t.buffer, m)
-	return t.buffer
+	b.buffer += n
+	b.buffer = math.Max(0, b.buffer)
+	b.buffer = math.Min(b.buffer, m)
+	return b.buffer
 }
 
 // TrackViolation ...
-func (t *basic) TrackViolation() {
-	t.violations++
+func (b *basic) TrackViolation() {
+	b.violations++
 }
 
 // Violations ...
-func (t *basic) Violations() float64 {
-	return t.violations
+func (b *basic) Violations() float64 {
+	return b.violations
 }
 
 // violationAfterTicks ...
-func (t *basic) violationAfterTicks(tick uint64, maxTicks uint64) float64 {
+func (b *basic) violationAfterTicks(tick uint64, maxTicks uint64) float64 {
 	defer func() {
-		t.lastFlagTick = tick
+		b.lastFlagTick = tick
 	}()
-	diff := float64(tick - t.lastFlagTick)
+	diff := float64(tick - b.lastFlagTick)
 	return math.Max(((float64(maxTicks)+math.Min(diff, 1))-diff)/float64(maxTicks), 0)
 }
