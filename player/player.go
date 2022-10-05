0d102c74dcdfc0e7b6ecb6ea17ec74e7f545bd29
ethaniccc <schooldocumentsandstuff@gmail.com>
2022-10-04 23:43:54 -0400

    remove test

--- player/player.go
+++ player/player.go
@@ -622,22 +622,15 @@ func (p *Player) ackEntitiesPos() {
 
 // startTicking ticks the player until the connection is closed.
 func (p *Player) startTicking() {
-	t := time.NewTicker(time.Second / 1000)
+	t := time.NewTicker(time.Second / 20)
 	defer t.Stop()
 
-	rT := 0
 	p.lastServerTicked = time.Now()
 	for {
 		select {
 		case <-p.c:
 			return
 		case <-t.C:
-			rT++
-			if rT%50 != 0 {
-				p.ackEntitiesPos()
-				p.flushConns()
-				continue
-			}
 			// This will prepare the entity positions to be acknowledged.
 			p.ackEntitiesPos()
 
