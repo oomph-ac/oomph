b413fae8b72f42bc2d32d3559f7559f3863c382f
ethaniccc <schooldocumentsandstuff@gmail.com>
2022-11-02 11:53:56 -0400

    ffs

---
+++ check/reach_a.go
@@ -0,0 +1,152 @@
+package check
+
+import (
+	"math"
+
+	"github.com/df-mc/dragonfly/server/block/cube/trace"
+	"github.com/go-gl/mathgl/mgl64"
+	"github.com/oomph-ac/oomph/game"
+	"github.com/oomph-ac/oomph/utils"
+	"github.com/sandertv/gophertunnel/minecraft/protocol"
+	"github.com/sandertv/gophertunnel/minecraft/protocol/packet"
+)
+
+const interpolatedFrames float64 = 4
+
+type ReachA struct {
+	eid             uint64
+	run, cancelNext bool
+	basic
+}
+
+func NewReachA() *ReachA {
+	return &ReachA{}
+}
+
+func (*ReachA) Name() (string, string) {
+	return "Reach", "A"
+}
+
+func (*ReachA) Description() string {
+	return "This checks if a player's combat range is invalid."
+}
+
+func (*ReachA) MaxViolations() float64 {
+	return math.MaxFloat64
+}
+
+func (r *ReachA) Process(p Processor, pk packet.Packet) bool {
+	if p.CombatMode() != utils.ModeSemiAuthoritative {
+		return false
+	}
+
+	if tpk, ok := pk.(*packet.InventoryTransaction); ok {
+		d, ok := tpk.TransactionData.(*protocol.UseItemOnEntityTransactionData)
+		if !ok {
+			return false
+		}
+
+		if d.ActionType != protocol.UseItemOnEntityActionAttack {
+			return false
+		}
+
+		if r.cancelNext {
+			r.cancelNext = false
+			return true
+		}
+
+		r.eid = d.TargetEntityRuntimeID
+		r.run = true
+	}
+
+	if i, ok := pk.(*packet.PlayerAuthInput); ok && r.run {
+		defer func() {
+			r.run = false
+		}()
+
+		if i.InputMode != packet.InputModeMouse {
+			return false
+		}
+
+		e, ok := p.SearchEntity(r.eid)
+		if !ok { // The entity does not exist and we cannot do the reach check.
+			return false
+		}
+
+		rewind := e.RewindPosition(p.ServerTick() - 3)
+		if rewind == nil {
+			return false
+		}
+
+		//bb := e.AABB().Translate(e.LastPosition()).Grow(0.1)
+		pe := p.Entity()
+
+		cDv, lDv := game.DirectionVector(pe.Rotation()[2], pe.Rotation()[0]),
+			game.DirectionVector(pe.LastRotation()[2], pe.LastRotation()[0])
+		cAtkPos, lAtkPos := pe.Position().Add(mgl64.Vec3{0, 1.62}), pe.LastPosition().Add(mgl64.Vec3{0, 1.62})
+		cEntPos, lEntPos := e.LastPosition(), rewind.Position
+		dvDelta, atkPosDelta, entPosDelta := cDv.Sub(lDv).Mul(1/interpolatedFrames),
+			cAtkPos.Sub(lAtkPos).Mul(1/interpolatedFrames), cEntPos.Sub(lEntPos).Mul(1/interpolatedFrames)
+		uAtkPos, uDv, uEntPos := lAtkPos, lDv, lEntPos
+
+		minDist, valid := 6900.0, false
+
+		// This will require for (interpolatedFrame ^ 3) raycasts to be performed.
+		// We need to interpolate the attack position and the direction vector.
+		for x := 0.0; x < interpolatedFrames; x++ {
+			if x != 0 {
+				uAtkPos = uAtkPos.Add(atkPosDelta)
+			}
+
+			for y := 0.0; y < interpolatedFrames; y++ {
+				if y != 0 {
+					uDv = uDv.Add(dvDelta)
+				}
+
+				for z := 0.0; z < interpolatedFrames; z++ {
+					if z != 0 {
+						uEntPos = uEntPos.Add(entPosDelta)
+					}
+
+					bb := e.AABB().Translate(uEntPos).Grow(0.1)
+					result, ok := trace.BBoxIntercept(bb, uAtkPos, uAtkPos.Add(uDv.Mul(7.0)))
+					if !ok {
+						continue
+					}
+
+					dist := result.Position().Sub(uAtkPos).Len()
+					if dist > 7 { // This is impossible as we only traversed 7 blocks.
+						continue
+					}
+
+					valid = true
+					if dist < minDist {
+						minDist = dist
+					}
+				}
+			}
+		}
+
+		if !valid {
+			r.cancelNext = true
+			return false
+		}
+
+		if minDist < 3.01 {
+			r.Buff(-0.0125)
+			r.violations -= math.Max(r.violations-0.002, 0)
+			return false
+		}
+
+		if r.Buff(1, 3.3) < 3 {
+			return false
+		}
+
+		p.Flag(r, 1, map[string]any{
+			"dist": game.Round(minDist, 2),
+		})
+		r.cancelNext = true
+	}
+
+	return false
+}
---
+++ check/reach_b.go
@@ -0,0 +1,71 @@
+package check
+
+import (
+	"github.com/go-gl/mathgl/mgl64"
+	"github.com/oomph-ac/oomph/game"
+	"github.com/oomph-ac/oomph/utils"
+	"github.com/sandertv/gophertunnel/minecraft/protocol"
+	"github.com/sandertv/gophertunnel/minecraft/protocol/packet"
+)
+
+type ReachB struct {
+	basic
+}
+
+func NewReachB() *ReachB {
+	return &ReachB{}
+}
+
+func (*ReachB) Name() (string, string) {
+	return "Reach", "B"
+}
+
+func (*ReachB) Description() string {
+	return "This checks if a player's combat is invalid by checking raw distance"
+}
+
+func (*ReachB) MaxViolations() float64 {
+	return 30
+}
+
+func (r *ReachB) Process(p Processor, pk packet.Packet) bool {
+	t, ok := pk.(*packet.InventoryTransaction)
+	if !ok {
+		return false
+	}
+
+	if p.CombatMode() != utils.ModeSemiAuthoritative {
+		return false
+	}
+
+	d, ok := t.TransactionData.(*protocol.UseItemOnEntityTransactionData)
+	if !ok {
+		return false
+	}
+
+	if d.ActionType != protocol.UseItemOnEntityActionAttack {
+		return false
+	}
+
+	e, ok := p.SearchEntity(d.TargetEntityRuntimeID)
+	if !ok {
+		return false
+	}
+
+	bb := e.AABB().Translate(e.Position()).Grow(0.1)
+	dist := game.AABBVectorDistance(bb, p.Entity().Position().Add(mgl64.Vec3{0, 1.62}))
+
+	if dist < 3.1 {
+		return false
+	}
+
+	if r.Buff(1, 10) < 5 {
+		return false
+	}
+
+	p.Flag(r, r.violationAfterTicks(p.ClientFrame(), 300), map[string]any{
+		"dist": game.Round(dist, 2),
+	})
+
+	return true
+}
