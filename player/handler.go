54ea345e963c5b88f305296b973b904dd2d6eaad
ethaniccc <schooldocumentsandstuff@gmail.com>
2023-05-22 11:24:39 -0400

    allow handler to handle packets

--- player/handler.go
+++ player/handler.go
@@ -3,6 +3,7 @@ package player
 import (
 	"github.com/df-mc/dragonfly/server/event"
 	"github.com/oomph-ac/oomph/check"
+	"github.com/sandertv/gophertunnel/minecraft/protocol/packet"
 )
 
 // Handler handles events that are called by a player. Implementations of Handler may be used to listen to
@@ -15,6 +16,10 @@ type Handler interface {
 	HandleFlag(ctx *event.Context, check check.Check, params map[string]any, log *bool)
 	// HandleDebug handles check debug messages. These are logged by default and ctx.Cancel should be used to cancel them.
 	HandleDebug(ctx *event.Context, check check.Check, params map[string]any)
+	// HandleClientPacket handles when a player sends a packet to oomph.
+	HandleClientPacket(ctx *event.Context, packet packet.Packet)
+	// HandleServerPacket handles when a player receives a packet from oomph.
+	HandleServerPacket(ctx *event.Context, packet packet.Packet)
 }
 
 // NopHandler implements the Handler interface but does not execute any code when an event is called. The
@@ -33,3 +38,9 @@ func (NopHandler) HandleFlag(*event.Context, check.Check, map[string]any, *bool)
 
 // HandleDebug ...
 func (NopHandler) HandleDebug(*event.Context, check.Check, map[string]any) {}
+
+// HandleClientPacket ...
+func (NopHandler) HandleClientPacket(*event.Context, packet.Packet) {}
+
+// HandleServerPacket ...
+func (NopHandler) HandleServerPacket(*event.Context, packet.Packet) {}
