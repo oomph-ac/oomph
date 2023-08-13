629343637563251252a054da7e7b8ca8f561fed0
ethaniccc <schooldocumentsandstuff@gmail.com>
2023-08-13 20:57:26 +0000

    only sync per second & make last attack nil

--- player/player.go
+++ player/player.go
@@ -769,7 +769,7 @@ func (p *Player) tryDoSync() {
 			p.clientTick.Store(sTick)
 			p.isSyncedWithServer = true
 			p.awaitingSync = false
-			p.nextSyncTick = p.ServerTick() + uint64(p.TickLatency()) + 1
+			p.nextSyncTick = p.ServerTick() + uint64(p.TickLatency()) + 20
 		})
 	}
 }
