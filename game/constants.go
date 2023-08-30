e52eec771d4f8b85a200ae299b71b71078e72b99
ethaniccc <schooldocumentsandstuff@gmail.com>
2023-08-30 19:11:56 -0400

    add correct bounce code for beds

--- game/constants.go
+++ game/constants.go
@@ -1,18 +1,13 @@
 package game
 
 const (
-	// DefaultJumpMotion is the default amount of motion for a jump.
-	DefaultJumpMotion = float32(0.42)
-	// DefaultAirFriction is the default air friction for a player
-	DefaultAirFriction = float32(0.91)
-	// GravityMultiplier is the multiplier for player gravity.
-	GravityMultiplier = float32(0.98)
-	// NormalGravity is the normal gravity for players.
-	NormalGravity = float32(0.08)
-	// SlowFallingGravity is the gravity for players who have the slow falling effect enabled.
-	SlowFallingGravity = float32(0.01)
-	// StepHeight is the maximum height a player can step up.
-	StepHeight = float32(0.6)
-	// StepClipMultiplier is the multiplier for the step height of a player while they are clipping into a stair.
-	StepClipMultiplier = float32(0.4)
+	DefaultJumpMotion     = float32(0.42)
+	DefaultAirFriction    = float32(0.91)
+	GravityMultiplier     = float32(0.98)
+	NormalGravity         = float32(0.08)
+	SlowFallingGravity    = float32(0.01)
+	StepHeight            = float32(0.6)
+	StepClipMultiplier    = float32(0.4)
+	SlimeBounceMultiplier = float32(-1)
+	BedBounceMultiplier   = float32(-0.66)
 )
