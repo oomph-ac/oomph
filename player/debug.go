4c9273b1067964f00bd04f9fe5d4b7b86cf2a0b9
ethaniccc <schooldocumentsandstuff@gmail.com>
2023-11-27 01:25:11 +0000

    fix step logic again ffs

--- player/debug.go
+++ player/debug.go
@@ -1,5 +1,17 @@
 package player
 
+import (
+	"fmt"
+
+	"github.com/sandertv/gophertunnel/minecraft/protocol/packet"
+)
+
+const (
+	DebugTypeChat = iota
+	DebugTypePopup
+	DebugTypeLogged
+)
+
 type Debugger struct {
 	LogLatency  bool
 	LogCombat   bool
@@ -11,4 +23,21 @@ type Debugger struct {
 	SmoothTeleports bool
 
 	AllowedDebug bool
+}
+
+func (p *Player) tryDebug(msg string, dtype int, log bool) {
+	if !log {
+		return
+	}
+
+	switch dtype {
+	case DebugTypeChat:
+		p.SendOomphDebug(msg, packet.TextTypeChat)
+	case DebugTypePopup:
+		p.SendOomphDebug(msg, packet.TextTypePopup)
+	case DebugTypeLogged:
+		p.Log().Debug(msg)
+	default:
+		panic(fmt.Errorf("unknown debug type %v", dtype))
+	}
 }
