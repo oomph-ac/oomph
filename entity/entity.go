f14e0feb7e73f504b1b903380cd1c226f833dc35
ethaniccc <schooldocumentsandstuff@gmail.com>
2024-01-04 14:54:14 +0000

    add detection base, still in progress...

--- entity/entity.go
+++ entity/entity.go
@@ -1,19 +1,27 @@
 package entity
 
-import "github.com/go-gl/mathgl/mgl32"
+import (
+	"github.com/ethaniccc/float32-cube/cube"
+	"github.com/go-gl/mathgl/mgl32"
+)
 
 const (
-	EntityPlayerInterpolationTicks = 3
+	EntityPlayerInterpolationTicks = 4
 	EntityMobInterpolationTicks    = 6
 )
 
 type Entity struct {
-	Position, PreviousPosition mgl32.Vec3
-	RecvPosition               mgl32.Vec3
-	PositionHistory            []HistoricalPosition
+	Position, PrevPosition mgl32.Vec3
+	Velocity, PrevVelocity mgl32.Vec3
+	RecvPosition           mgl32.Vec3
 
-	HistorySize        int
+	HistorySize     int
+	PositionHistory []HistoricalPosition
+
 	InterpolationTicks int
+
+	Width  float32
+	Height float32
 
 	IsPlayer bool
 }
@@ -21,9 +29,12 @@ type Entity struct {
 // New creates and returns a new Entity instance.
 func New(pos, vel mgl32.Vec3, historySize int, isPlayer bool) *Entity {
 	e := &Entity{
-		Position:         pos,
-		PreviousPosition: pos,
-		RecvPosition:     pos.Add(vel),
+		Position:     pos,
+		PrevPosition: pos,
+		RecvPosition: pos.Add(vel),
+
+		Width:  0.6,
+		Height: 1.8,
 
 		HistorySize: historySize,
 		IsPlayer:    isPlayer,
@@ -48,17 +59,21 @@ func (e *Entity) RecievePosition(hp HistoricalPosition) {
 
 // UpdatePosition updates the position of the entity, and adds the previous position to the position history.
 func (e *Entity) UpdatePosition(hp HistoricalPosition) {
-	e.PreviousPosition = e.Position
+	e.PrevPosition = e.Position
 	e.Position = hp.Position
 
-	e.PositionHistory = append(e.PositionHistory, HistoricalPosition{
-		Position: e.PreviousPosition,
-		Tick:     hp.Tick,
-	})
+	e.PrevVelocity = e.Velocity
+	e.Velocity = e.Position.Sub(e.PrevPosition)
 
+	e.PositionHistory = append(e.PositionHistory, hp)
 	if len(e.PositionHistory) > e.HistorySize {
 		e.PositionHistory = e.PositionHistory[1:]
 	}
+}
+
+// Box returns the entity's bounding box.
+func (e *Entity) Box(pos mgl32.Vec3) cube.BBox {
+	return cube.Box(-e.Width/2, 0, -e.Width/2, e.Width/2, e.Height, e.Width/2).Translate(pos)
 }
 
 // Tick updates the entity's position based on the interpolation ticks.
