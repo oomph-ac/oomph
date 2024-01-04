64828b836b824c2e656dbd01ea40b45efc65ec50
ethaniccc <schooldocumentsandstuff@gmail.com>
2024-01-04 18:25:46 +0000

    finish up flagging and punishments

--- player/handler.go
+++ player/handler.go
@@ -1,6 +1,10 @@
 package player
 
-import "github.com/sandertv/gophertunnel/minecraft/protocol/packet"
+import (
+	"github.com/df-mc/dragonfly/server/event"
+	"github.com/elliotchance/orderedmap/v2"
+	"github.com/sandertv/gophertunnel/minecraft/protocol/packet"
+)
 
 // Handler is an interface that can be implemented to have the player handle certain packets and events.
 type Handler interface {
@@ -15,4 +19,22 @@ type Handler interface {
 	HandleServerPacket(pk packet.Packet, p *Player) bool
 	// OnTick is called every server tick.
 	OnTick(p *Player)
+}
+
+// EventHandler is an interface that can be implemented to have the player handle certain events.
+type EventHandler interface {
+	// OnPunishment is called when a detection triggers a punishment for a player.
+	OnPunishment(ctx *event.Context, p *Player, message *string)
+	// OnFlagged is called when a detection flags a player.
+	OnFlagged(ctx *event.Context, p *Player, detection Handler, data *orderedmap.OrderedMap[string, any])
+}
+
+// NopEventHandler is an event handler that does nothing.
+type NopEventHandler struct {
+}
+
+func (NopEventHandler) OnPunishment(ctx *event.Context, p *Player, message *string) {
+}
+
+func (NopEventHandler) OnFlagged(ctx *event.Context, p *Player, detection Handler, data *orderedmap.OrderedMap[string, any]) {
 }
