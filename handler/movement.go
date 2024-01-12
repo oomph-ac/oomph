48ccbf1d601beeb360ef1d17244c0e96dc95f784
ethaniccc <schooldocumentsandstuff@gmail.com>
2024-01-12 23:10:35 +0000

    only need to impl the movement simulation now...

--- handler/movement.go
+++ handler/movement.go
@@ -3,8 +3,12 @@ package handler
 import (
 	"github.com/ethaniccc/float32-cube/cube"
 	"github.com/go-gl/mathgl/mgl32"
+	"github.com/oomph-ac/oomph/entity"
+	"github.com/oomph-ac/oomph/game"
+	"github.com/oomph-ac/oomph/oerror"
 	"github.com/oomph-ac/oomph/player"
 	"github.com/oomph-ac/oomph/utils"
+	"github.com/sandertv/gophertunnel/minecraft/protocol"
 	"github.com/sandertv/gophertunnel/minecraft/protocol/packet"
 )
 
@@ -13,16 +17,27 @@ const HandlerIDMovement = "oomph:movement"
 type MovementHandler struct {
 	BoundingBox                        cube.BBox
 	Position, PrevPosition             mgl32.Vec3
+	Velocity, PrevVelocity             mgl32.Vec3
 	ClientPosition, PrevClientPosition mgl32.Vec3
 	ClientVel, PrevClientVel           mgl32.Vec3
 
 	// Rotation vectors are formatted as {pitch, headYaw, yaw}
 	Rotation, PrevRotation mgl32.Vec3
 
-	Sneaking  bool
-	Sprinting bool
+	ForwardImpulse float32
+	LeftImpulse    float32
+
+	Sneaking        bool
+	SneakKeyPressed bool
+
+	Sprinting        bool
+	SprintKeyPressed bool
+
+	Gravity float32
 
 	Jumping            bool
+	JumpKeyPressed     bool
+	JumpHeight         float32
 	TicksUntilNextJump int
 
 	Knockback           mgl32.Vec3
@@ -32,14 +47,22 @@ type MovementHandler struct {
 	SmoothTeleport     bool
 	TicksSinceTeleport int
 
+	Flying         bool
+	ToggledFly     bool
+	TrustFlyStatus bool
+
 	// MovementSpeed is the current movement speed of the player.
 	MovementSpeed float32
+	AirSpeed      float32
 	// HasServerSpeed is false when the client does an action that changes their movement speed, but
 	// has not recieved data from the server if their speed was actually modified. It is set to true when
 	// the client acknowledges the change in speed.
 	HasServerSpeed bool
 	// ClientPredictsSpeed is set manually by the end-user, and is set to truew
 	ClientPredictsSpeed bool
+
+	// s is the simulator that will be used for movement simulations. It can be set via. UseSimulator()
+	s Simulator
 }
 
 func NewMovementHandler() *MovementHandler {
@@ -75,14 +98,53 @@ func (h *MovementHandler) HandleClientPacket(pk packet.Packet, p *player.Player)
 	h.Rotation = mgl32.Vec3{input.Pitch, input.HeadYaw, input.Yaw}
 
 	h.updateMovementStates(p, input)
+	if h.s == nil {
+		panic(oerror.New("simulator not set in movement handler"))
+	}
 
-	// TODO: Movement simulation.
-
+	h.s.Simulate(p)
 	return true
 }
 
 func (h *MovementHandler) HandleServerPacket(pk packet.Packet, p *player.Player) bool {
 	switch pk := pk.(type) {
+	case *packet.SetActorData:
+		if pk.EntityRuntimeID != p.RuntimeId {
+			return true
+		}
+
+		p.Handler(HandlerIDAcknowledgements).(*AcknowledgementHandler).AddCallback(func() {
+			width, widthExists := pk.EntityMetadata[entity.DataKeyBoundingBoxWidth]
+			height, heightExists := pk.EntityMetadata[entity.DataKeyBoundingBoxHeight]
+
+			if !widthExists {
+				width = h.BoundingBox.Width() / 2
+			}
+
+			if !heightExists {
+				height = h.BoundingBox.Height()
+			}
+
+			h.BoundingBox = cube.Box(
+				-width.(float32)/2,
+				0,
+				-width.(float32)/2,
+				width.(float32)/2,
+				height.(float32),
+				width.(float32)/2,
+			)
+		})
+	case *packet.UpdateAttributes:
+		if pk.EntityRuntimeID != p.RuntimeId {
+			return true
+		}
+
+		p.Handler(HandlerIDAcknowledgements).(*AcknowledgementHandler).AddCallback(func() {
+			h.handleAttribute("minecraft:movement", pk.Attributes, func(attr protocol.Attribute) {
+				h.HasServerSpeed = true
+				h.MovementSpeed = float32(attr.Value)
+			})
+		})
 	case *packet.SetActorMotion:
 		if pk.EntityRuntimeID != p.RuntimeId {
 			return true
@@ -92,9 +154,6 @@ func (h *MovementHandler) HandleServerPacket(pk packet.Packet, p *player.Player)
 			h.knockback(pk.Velocity)
 		})
 	case *packet.MovePlayer:
-		// Prevent client-side rewinding.
-		pk.Tick = 0
-
 		if pk.EntityRuntimeID != p.RuntimeId {
 			return true
 		}
@@ -132,6 +191,19 @@ func (*MovementHandler) OnTick(p *player.Player) {
 func (*MovementHandler) Defer() {
 }
 
+func (h *MovementHandler) UseSimulator(s Simulator) {
+	h.s = s
+}
+
+func (h *MovementHandler) handleAttribute(n string, list []protocol.Attribute, f func(protocol.Attribute)) {
+	for _, attr := range list {
+		if attr.Name == n {
+			f(attr)
+			return
+		}
+	}
+}
+
 // calculateClientSpeed calculates the speed of the client when it is predicting its own speed.
 func (h *MovementHandler) calculateClientSpeed(p *player.Player) (speed float32) {
 	speed = float32(0.1)
@@ -166,6 +238,9 @@ func (h *MovementHandler) knockback(kb mgl32.Vec3) {
 }
 
 func (h *MovementHandler) updateMovementStates(p *player.Player, pk *packet.PlayerAuthInput) {
+	h.ForwardImpulse = pk.MoveVector.Y() * 0.98
+	h.LeftImpulse = pk.MoveVector.X() * 0.98
+
 	startFlag, stopFlag := utils.HasFlag(pk.InputData, packet.InputFlagStartSprinting), utils.HasFlag(pk.InputData, packet.InputFlagStopSprinting)
 	needsSpeedAdjustment := false
 	if startFlag && stopFlag {
@@ -181,10 +256,28 @@ func (h *MovementHandler) updateMovementStates(p *player.Player, pk *packet.Play
 		h.Sprinting = false
 		needsSpeedAdjustment = !h.HasServerSpeed
 	}
+	h.SprintKeyPressed = utils.HasFlag(pk.InputData, packet.InputFlagSprinting)
+
+	if utils.HasFlag(pk.InputData, packet.InputFlagStartSneaking) {
+		h.Sneaking = true
+	} else if utils.HasFlag(pk.InputData, packet.InputFlagStopSneaking) {
+		h.Sneaking = false
+	}
+	h.SneakKeyPressed = utils.HasFlag(pk.InputData, packet.InputFlagSneaking)
+
+	h.Jumping = utils.HasFlag(pk.InputData, packet.InputFlagStartJumping)
+	h.JumpKeyPressed = utils.HasFlag(pk.InputData, packet.InputFlagJumping)
+	h.JumpHeight = game.DefaultJumpHeight
 
 	// If a speed adjustment is needed, calculate the new speed of the client.
 	if needsSpeedAdjustment {
 		h.MovementSpeed = h.calculateClientSpeed(p)
+	}
+
+	h.Gravity = game.NormalGravity
+	h.AirSpeed = 0.02
+	if h.Sprinting {
+		h.AirSpeed += 0.006
 	}
 
 	// Update the amount of ticks since actions.
