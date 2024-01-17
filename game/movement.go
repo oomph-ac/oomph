118fc625e1199b4dfe8c771efa1ae7044090b8c8
ethaniccc <schooldocumentsandstuff@gmail.com>
2024-01-17 00:59:55 +0000

    cadet go fix big brain dev pls

--- game/movement.go
+++ game/movement.go
@@ -3,6 +3,7 @@ package game
 const (
 	DefaultJumpHeight     = float32(0.42)
 	DefaultAirFriction    = float32(0.91)
+	DefaultBlockFriction  = float32(0.6)
 	GravityMultiplier     = float32(0.98)
 	NormalGravity         = float32(0.08)
 	SlowFallingGravity    = float32(0.01)
@@ -10,6 +11,7 @@ const (
 	StepClipMultiplier    = float32(0.4)
 	SlimeBounceMultiplier = float32(-1)
 	BedBounceMultiplier   = float32(-0.66)
+	ClimbSpeed            = float32(0.2)
 
 	JumpDelayTicks = 10
 )
