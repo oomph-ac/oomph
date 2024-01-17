9987dcd870e95ff81a4b812b32f4a392d3370d67
ethaniccc <schooldocumentsandstuff@gmail.com>
2024-01-17 14:17:43 +0000

    account for client side block breaking

--- handler/chunks.go
+++ handler/chunks.go
@@ -7,11 +7,15 @@ import (
 
 	"github.com/chewxy/math32"
 	"github.com/df-mc/dragonfly/server/block"
+	df_cube "github.com/df-mc/dragonfly/server/block/cube"
 	df_world "github.com/df-mc/dragonfly/server/world"
 	"github.com/df-mc/dragonfly/server/world/chunk"
 	"github.com/ethaniccc/float32-cube/cube"
+	"github.com/go-gl/mathgl/mgl32"
+	"github.com/oomph-ac/oomph/game"
 	"github.com/oomph-ac/oomph/oerror"
 	"github.com/oomph-ac/oomph/player"
+	"github.com/oomph-ac/oomph/utils"
 	"github.com/oomph-ac/oomph/world"
 	"github.com/sandertv/gophertunnel/minecraft/protocol"
 	"github.com/sandertv/gophertunnel/minecraft/protocol/packet"
@@ -20,10 +24,11 @@ import (
 const HandlerIDChunks = "oomph:chunks"
 
 type ChunksHandler struct {
-	World       *world.World
-	ChunkRadius int32
-
+	World         *world.World
+	ChunkRadius   int32
 	InLoadedChunk bool
+
+	breakingBlockPos *protocol.BlockPos
 }
 
 func NewChunksHandler() *ChunksHandler {
@@ -38,14 +43,121 @@ func (h *ChunksHandler) ID() string {
 }
 
 func (h *ChunksHandler) HandleClientPacket(pk packet.Packet, p *player.Player) bool {
+	if p.MovementMode == player.AuthorityModeNone {
+		return true
+	}
+
 	switch pk := pk.(type) {
 	case *packet.TickSync:
 		h.ChunkRadius = p.ServerConn().GameData().ChunkRadius
+	case *packet.InventoryTransaction:
+		dat, ok := pk.TransactionData.(*protocol.UseItemTransactionData)
+		if !ok {
+			return true
+		}
+
+		i, ok := df_world.ItemByRuntimeID(dat.HeldItem.Stack.NetworkID, int16(dat.HeldItem.Stack.MetadataValue))
+		if !ok {
+			return true
+		}
+
+		// Determine if the item can be placed as a block.
+		b, ok := i.(df_world.Block)
+		if !ok {
+			return true
+		}
+
+		// Find the replace position of the block. This will be used if the block at the current position
+		// is replacable (e.g: water, lava, air).
+		replacePos := utils.BlockToCubePos(dat.BlockPosition)
+		fb := h.World.GetBlock(replacePos)
+
+		// If the block at the position is not replacable, we want to place the block on the side of the block.
+		if replaceable, ok := fb.(block.Replaceable); !ok || !replaceable.ReplaceableBy(b) {
+			replacePos = replacePos.Side(cube.Face(dat.BlockFace))
+		}
+
+		// Make a list of BBoxes the block will occupy.
+		bx := b.Model().BBox(df_cube.Pos(replacePos), nil)
+		boxes := make([]cube.BBox, 0)
+		for _, bxx := range bx {
+			// Don't continue if the block isn't 1x1x1.
+			// TODO: Implement placements for these blocks properly.
+			if bxx.Width() != 1 || bxx.Height() != 1 || bxx.Length() != 1 {
+				return true
+			}
+
+			boxes = append(boxes, game.DFBoxToCubeBox(bxx).Translate(mgl32.Vec3{
+				float32(replacePos.X()),
+				float32(replacePos.Y()),
+				float32(replacePos.Z()),
+			}))
+		}
+
+		// Get the player's AABB and translate it to the position of the player. Then check if it intersects
+		// with any of the boxes the block will occupy. If it does, we don't want to place the block.
+		movHandler := p.Handler(HandlerIDMovement).(*MovementHandler)
+		if cube.AnyIntersections(boxes, movHandler.BoundingBox()) {
+			return true
+		}
+
+		entHandler := p.Handler(HandlerIDEntities).(*EntitiesHandler)
+		for _, e := range entHandler.Entities {
+			if cube.AnyIntersections(boxes, e.Box(e.Position)) {
+				return true
+			}
+		}
+
+		// Set the block in the world.
+		h.World.SetBlock(replacePos, b)
+		return true
 	case *packet.PlayerAuthInput:
 		// TODO: Use server position on full authority mode.
 		chunkPos := protocol.ChunkPos{
 			int32(math32.Floor(pk.Position.X())) >> 4,
 			int32(math32.Floor(pk.Position.Z())) >> 4,
+		}
+
+		if utils.HasFlag(pk.InputData, packet.InputFlagPerformBlockActions) {
+			for _, action := range pk.BlockActions {
+				switch action.Action {
+				case protocol.PlayerActionPredictDestroyBlock:
+					h.World.SetBlock(cube.Pos{
+						int(action.BlockPos.X()),
+						int(action.BlockPos.Y()),
+						int(action.BlockPos.Z()),
+					}, block.Air{})
+				case protocol.PlayerActionStartBreak:
+					if h.breakingBlockPos != nil {
+						continue
+					}
+
+					h.breakingBlockPos = &action.BlockPos
+				case protocol.PlayerActionCrackBreak:
+					if h.breakingBlockPos == nil {
+						continue
+					}
+
+					if *h.breakingBlockPos == action.BlockPos {
+						continue
+					}
+
+					panic(oerror.New("invalid block action recieved [1]"))
+				case protocol.PlayerActionAbortBreak:
+					h.breakingBlockPos = nil
+				case protocol.PlayerActionStopBreak:
+					if h.breakingBlockPos == nil {
+						continue
+					}
+
+					h.World.SetBlock(cube.Pos{
+						int(action.BlockPos.X()),
+						int(action.BlockPos.Y()),
+						int(action.BlockPos.Z()),
+					}, block.Air{})
+					h.breakingBlockPos = nil
+				}
+			}
 		}
 
 		h.World.CleanChunks(h.ChunkRadius, chunkPos)
