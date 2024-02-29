b53679e6e03b575f0f69f0cbab1f28d3b86fdd68
ethaniccc <ethaniccc@skiff.com>
2024-02-29 14:35:10 +0000

    remove debug

--- oomph.go
+++ oomph.go
@@ -30,7 +30,6 @@ func init() {
 		Dsn:           "https://06f2165840f341138a676b52eacad19c@o1409396.ingest.sentry.io/6747367",
 		EnableTracing: os.Getenv("SENTRY_TRACE") == "true",
 		TracesSampler: sentry.TracesSampler(func(ctx sentry.SamplingContext) float64 {
-			fmt.Println(ctx.Span.Name)
 			if ctx.Span.Name != "oomph:handle_client" && ctx.Span.Name != "oomph:handle_server" && ctx.Span.Name != "oomph:tick" {
 				return 0.0
 			}
