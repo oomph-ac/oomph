629e30a6804ef38adeb71b9aaf622e268d8347b9
GameParrot <85067619+GameParrot@users.noreply.github.com>
2024-03-04 20:36:43 -0500

    use anti ddos raknet, server nuke a and b

---
+++ detection/server_nuke_b.go
@@ -0,0 +1,45 @@
+package detection
+
+import (
+	"github.com/oomph-ac/oomph/player"
+	"github.com/sandertv/gophertunnel/minecraft/protocol/packet"
+)
+
+const DetectionIDServerNukeB = "oomph:server_nuke_b"
+
+type ServerNukeB struct {
+	BaseDetection
+}
+
+func NewServerNukeB() *ServerNukeB {
+	d := &ServerNukeB{}
+	d.Type = "ServerNuke"
+	d.SubType = "B"
+
+	d.Description = "Checks if a player is sending text packet jukebox popup (usually combined with large parameters array to lag servers)."
+	d.Punishable = true // should ip ban when this flags
+
+	d.MaxViolations = 1
+	d.trustDuration = -1
+
+	d.FailBuffer = 0
+	d.MaxBuffer = 1
+	return d
+}
+
+func (d *ServerNukeB) ID() string {
+	return DetectionIDServerNukeB
+}
+
+func (d *ServerNukeB) HandleClientPacket(pk packet.Packet, p *player.Player) bool {
+	i, ok := pk.(*packet.Text)
+	if !ok {
+		return true
+	}
+
+	if i.TextType == packet.TextTypeJukeboxPopup {
+		d.Fail(p, nil)
+	}
+
+	return true
+}
