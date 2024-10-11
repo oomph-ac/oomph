1422569f59512d1a286306be8e2b2474112751bd
ethaniccc <schooldocumentsandstuff@gmail.com>
2024-10-11 11:45:36 -0400

    progress with components

--- player/movement.go
+++ player/movement.go
@@ -10,27 +10,28 @@ import (
 // It is used for updating movement states of the player, and providing them to any other
 // component that requires it.
 type MovementComponent interface {
-	// CanSimulate returns true if the movement component can be simulated by the server for the current frame.
-	CanSimulate() bool
-	// SetCanSimulate sets wether or not the movement component can be simulated by the server for the current frame.
-	SetCanSimulate(simulate bool)
-
 	// Pos returns the position of the movement component.
 	Pos() mgl32.Vec3
 	// LastPos returns the previous position of the movement component.
 	LastPos() mgl32.Vec3
 	// SetPos sets the position of the movement component.
 	SetPos(pos mgl32.Vec3)
-	// PosDelta returns the difference between the current and previous position
-	// of the movement component.
-	PosDelta() mgl32.Vec3
 
-	// Vel sets the velocity of the movement component.
+	// Vel returns the velocity of the movement component.
 	Vel() mgl32.Vec3
 	// LastVel returns the previous velocity of the movement component.
 	LastVel() mgl32.Vec3
 	// SetVel returns the velocity of the movement component.
 	SetVel(vel mgl32.Vec3)
+
+	// Mov returns the velocity of the movement component before friction and
+	// gravity are applied to it.
+	Mov() mgl32.Vec3
+	// LastMov returns the previous processed velocity before friction and gravity
+	// of the movement component.
+	LastMov() mgl32.Vec3
+	// SetMov sets the velocity of the movement component before friction and gravity.
+	SetMov(mov mgl32.Vec3)
 
 	// Rotation returns the rotation of the movement component. The X-axis contains
 	// the pitch, the Y-axis contains the head-yaw, and the Z-axis contains the yaw.
@@ -63,14 +64,14 @@ type MovementComponent interface {
 	// SetPressingJump sets wether or not the movement component is holding down the key bound to the jump action.
 	SetPressingJump(pressing bool)
 	// JumpDelay returns the number of ticks until the movement component can make another jump.
-	JumpDelay() int64
+	JumpDelay() uint64
 	// SetJumpDelay sets the number of ticks until the movement component can make another jump.
-	SetJumpDelay(ticks int64)
+	SetJumpDelay(ticks uint64)
 
 	// Sneaking returns true if the movement component is currently sneaking.
 	Sneaking() bool
 	// SetSneaking sets wether or not the movement component is currently sneaking.
-	SetSneaking(bool)
+	SetSneaking(sneaking bool)
 	// PressingSneak returns true if the movement component is holding down the key bound to the sneak action.
 	PressingSneak() bool
 	// SetPressingSneak sets if the movement component is holding down the key bound o the sneak action.
@@ -89,20 +90,19 @@ type MovementComponent interface {
 	// not support one-way collisions.
 	SetStuckInCollider(stuck bool)
 
-	// NoClip returns true if the movement component is able to no-clip.
-	NoClip() bool
-	// SetNoClip sets wether or not the movement component is able to no-clip.
-	SetNoClip(noClip bool)
-
 	// Knockback returns the knockback sent by the server to the movement component.
 	Knockback() mgl32.Vec3
 	// SetKnockback notifies the movement component of knockback sent by the server.
 	SetKnockback(vel mgl32.Vec3)
+	// HasKnockback returns true if the movement component needs knockback applied on the next simulation.
+	HasKnockback() bool
 
 	// Teleport notifies the movement component of a teleport.
 	Teleport(pos mgl32.Vec3, onGround bool, smoothed bool)
 	// TeleportPos returns the teleport position sent to the movement component.
 	TeleportPos() mgl32.Vec3
+	// HasTeleport returns true if the movement component needs a teleport applied on the next simulation.
+	HasTeleport() bool
 
 	// Size returns the width and height of the movement component in a Vec2. The X-axis
 	// contains the width, and the Y-axis contains the height.
@@ -113,7 +113,60 @@ type MovementComponent interface {
 	// it's current position.
 	BoundingBox() cube.BBox
 
-	// Update is a function that updates the states of the movement component based on the
-	// client's input for the current simulation frame.
+	// Gravity returns the gravity of the movement component.
+	Gravity() float32
+	// SetGravity sets the gravity of the movement component.
+	SetGravity(gravity float32)
+
+	// MovementSpeed returns the movement speed of the movement component.
+	MovementSpeed() float32
+	// SetMovementSpeed sets the movement speed of the movement component.
+	SetMovementSpeed(speed float32)
+
+	// AirSpeed returns the movement speed of the movement component while off ground.
+	AirSpeed() float32
+	// SetAirSpeed sets the movement speed of the movement component while off ground.
+	SetAirSpeed(airSpeed float32)
+
+	// JumpHeight returns the jump height of the movement component.
+	JumpHeight() float32
+	// SetJumpHeight sets the jump height of the movement component.
+	SetJumpHeight(height float32)
+
+	// XCollision returns true if the movement component is collided with a block
+	// on the x-axis.
+	XCollision() bool
+	// YCollision returns true if the movement component is collided with a block
+	// on the y-axis.
+	YCollision() bool
+	// ZCollision returns true if the movement component is collided with a block
+	// on the z-axis.
+	ZCollision() bool
+	// SetCollisions sets wether or not the movement component is colliding with a block
+	// on the x, y, or z axies.
+	SetCollisions(x, y, z bool)
+
+	// OnGround returns true if the movement component is on the ground.
+	OnGround() bool
+	// SetOnGround sets wether or not the movement component is on the ground.
+	SetOnGround(onGround bool)
+
+	// NoClientPredictions returns true if the movement component does not need their movement simulated.
+	NoClientPredictions() bool
+	// SetNoClientPredictions sets wether or not the movement component needxs their movement simulated.
+	SetNoClientPredictions(noClip bool)
+
+	// CanSimulate returns true if the movement component can be simulated by the server for the current frame.
+	CanSimulate() bool
+	// SetCanSimulate sets wether or not the movement component can be simulated by the server for the current frame.
+	SetCanSimulate(simulate bool)
+
+	// Update updates the states of the movement component from the given input.
 	Update(input *packet.PlayerAuthInput)
+	// Simulate does any simulations needed by the movement component.
+	Simulate()
+
+	// Validate is a function that returns true if this movement component has a position within
+	// the given threshold of the other movement component.
+	Validate(threshold float32, other MovementComponent) bool
 }
