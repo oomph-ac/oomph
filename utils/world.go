40fdff9f2bac5657a20c36f87004e9506ac13e61
Doge <cooldogedev@gmail.com>
2025-01-30 17:44:50 +0300

    world: added `glow_lichen` and `pink_petals` bounding boxes

--- utils/world.go
+++ utils/world.go
@@ -118,6 +118,8 @@ func BlockBoxes(b world.Block, pos cube.Pos, w *oomph_world.World) []cube.BBox {
 		return []cube.BBox{cube.Box(0, 0, 0, 1, 13.0/16.0, 1)}
 	case "minecraft:red_mushroom", "minecraft:brown_mushroom":
 		return []cube.BBox{}
+	case "minecraft:glow_lichen", "minecraft:pink_petals":
+		return []cube.BBox{}
 	default:
 		switch oldModel := b.Model().(type) {
 		case model.Wall:
