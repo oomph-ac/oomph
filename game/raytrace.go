cd9e2ebdb6b79c5aee670391331f916871b580f6
Doge <cooldogedev@gmail.com>
2025-01-30 04:14:51 +0300

    game/raytrace.go: use iterators for `BlocksBetween()` (#52)
    (cherry picked from commit 83fb83b17bf9bb3a00009e2f8cf6d55c3242de4c)
    (cherry picked from commit 3090f664cdd4bd0cd942e0b6c7ac2dae9cf6a0f8)

--- game/raytrace.go
+++ game/raytrace.go
@@ -1,69 +1,72 @@
 package game
 
 import (
+	"iter"
+
 	"github.com/chewxy/math32"
 	"github.com/ethaniccc/float32-cube/cube"
 	"github.com/go-gl/mathgl/mgl32"
 )
 
 // https://github.com/pmmp/Math/blob/stable/src/VoxelRayTrace.php#L67
-func BlocksBetween(start, end mgl32.Vec3) []mgl32.Vec3 {
-	dirVec := end.Sub(start).Normalize()
-	if dirVec.LenSqr() <= 0 {
-		return []mgl32.Vec3{}
-	}
+func BlocksBetween(start, end mgl32.Vec3) iter.Seq[mgl32.Vec3] {
+	return func(yield func(mgl32.Vec3) bool) {
+		dirVec := end.Sub(start).Normalize()
+		if dirVec.LenSqr() <= 0 {
+			return
+		}
 
-	radius := start.Sub(end).Len()
-	stepX := PHPSpaceshipOp(dirVec.X(), 0)
-	stepY := PHPSpaceshipOp(dirVec.Y(), 0)
-	stepZ := PHPSpaceshipOp(dirVec.Z(), 0)
+		radius := start.Sub(end).Len()
+		stepX := PHPSpaceshipOp(dirVec.X(), 0)
+		stepY := PHPSpaceshipOp(dirVec.Y(), 0)
+		stepZ := PHPSpaceshipOp(dirVec.Z(), 0)
 
-	tMaxX := rayTraceDistanceToBoundary(start.X(), dirVec.X())
-	tMaxY := rayTraceDistanceToBoundary(start.Y(), dirVec.Y())
-	tMaxZ := rayTraceDistanceToBoundary(start.Z(), dirVec.Z())
+		tMaxX := rayTraceDistanceToBoundary(start.X(), dirVec.X())
+		tMaxY := rayTraceDistanceToBoundary(start.Y(), dirVec.Y())
+		tMaxZ := rayTraceDistanceToBoundary(start.Z(), dirVec.Z())
 
-	tDeltaX := float32(0)
-	if dirVec.X() != 0 {
-		tDeltaX = stepX / dirVec.X()
-	}
+		tDeltaX := float32(0)
+		if dirVec.X() != 0 {
+			tDeltaX = stepX / dirVec.X()
+		}
 
-	tDeltaY := float32(0)
-	if dirVec.Y() != 0 {
-		tDeltaY = stepY / dirVec.Y()
-	}
+		tDeltaY := float32(0)
+		if dirVec.Y() != 0 {
+			tDeltaY = stepY / dirVec.Y()
+		}
 
-	tDeltaZ := float32(0)
-	if dirVec.Z() != 0 {
-		tDeltaZ = stepZ / dirVec.Z()
-	}
+		tDeltaZ := float32(0)
+		if dirVec.Z() != 0 {
+			tDeltaZ = stepZ / dirVec.Z()
+		}
 
-	blocks := []mgl32.Vec3{}
-	currentBlock := cube.PosFromVec3(start).Vec3()
-	for {
-		blocks = append(blocks, currentBlock)
-
-		if tMaxX < tMaxY && tMaxX < tMaxZ {
-			if tMaxX > radius {
-				break
+		currentBlock := cube.PosFromVec3(start).Vec3()
+		for {
+			if !yield(currentBlock) {
+				return
 			}
-			currentBlock = currentBlock.Add(mgl32.Vec3{stepX})
-			tMaxX += tDeltaX
-		} else if tMaxY < tMaxZ {
-			if tMaxY > radius {
-				break
+
+			if tMaxX < tMaxY && tMaxX < tMaxZ {
+				if tMaxX > radius {
+					return
+				}
+				currentBlock = currentBlock.Add(mgl32.Vec3{stepX})
+				tMaxX += tDeltaX
+			} else if tMaxY < tMaxZ {
+				if tMaxY > radius {
+					return
+				}
+				currentBlock = currentBlock.Add(mgl32.Vec3{0, stepY})
+				tMaxY += tDeltaY
+			} else {
+				if tMaxZ > radius {
+					return
+				}
+				currentBlock = currentBlock.Add(mgl32.Vec3{0, 0, stepZ})
+				tMaxZ += tDeltaZ
 			}
-			currentBlock = currentBlock.Add(mgl32.Vec3{0, stepY})
-			tMaxY += tDeltaY
-		} else {
-			if tMaxZ > radius {
-				break
-			}
-			currentBlock = currentBlock.Add(mgl32.Vec3{0, 0, stepZ})
-			tMaxZ += tDeltaZ
 		}
 	}
-
-	return blocks
 }
 
 // https://github.com/pmmp/Math/blob/stable/src/VoxelRayTrace.php#L134
