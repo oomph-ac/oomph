74b9ea1db04bb68871fc6ba7fcde1145837795d0
ethaniccc <schooldocumentsandstuff@gmail.com>
2025-02-19 17:25:11 -0500

    add missing file

---
+++ world/init.go
@@ -0,0 +1,24 @@
+package world
+
+import (
+	_ "unsafe"
+
+	"github.com/df-mc/dragonfly/server/world/chunk"
+	"github.com/oomph-ac/oomph/oerror"
+)
+
+var AirRuntimeID uint32
+
+// noinspection ALL
+//
+//go:linkname world_finaliseBlockRegistry github.com/df-mc/dragonfly/server/world.finaliseBlockRegistry
+func world_finaliseBlockRegistry()
+
+func init() {
+	world_finaliseBlockRegistry()
+	airRID, ok := chunk.StateToRuntimeID("minecraft:air", nil)
+	if !ok {
+		panic(oerror.New("unable to find runtime ID for air"))
+	}
+	AirRuntimeID = airRID
+}
