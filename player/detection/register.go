8a7b0726f56cb06ad6485451df4cea1fdc919344
ethaniccc <schooldocumentsandstuff@gmail.com>
2025-03-24 13:17:22 -0400

    stop registering this detection twice wtf??

--- player/detection/register.go
+++ player/detection/register.go
@@ -9,7 +9,6 @@ func Register(p *player.Player) {
 	// bad packet detections
 	p.RegisterDetection(New_BadPacketA(p))
 	p.RegisterDetection(New_BadPacketB(p))
-	p.RegisterDetection(New_BadPacketB(p))
 
 	// edition faker detections
 	p.RegisterDetection(New_EditionFakerA(p))
