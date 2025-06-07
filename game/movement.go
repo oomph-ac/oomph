a262a3df2a88ed34bf311b7103689230677f9c71
ethaniccc <ethaniccc@oomph.ac>
2025-06-06 21:01:21 -0400

    fixes #66

--- game/movement.go
+++ game/movement.go
@@ -1,16 +1,17 @@
 package game
 
 const (
-	DefaultJumpHeight     = float32(0.42)
-	DefaultAirFriction    = float32(0.91)
-	DefaultBlockFriction  = float32(0.6)
-	GravityMultiplier     = float32(0.98)
-	NormalGravity         = float32(0.08)
-	SlowFallingGravity    = float32(0.01)
-	StepHeight            = float32(0.6)
-	SlideOffsetMultiplier = float32(0.4)
-	SlimeBounceMultiplier = float32(-1)
-	BedBounceMultiplier   = float32(-0.66)
+	DefaultJumpHeight           = float32(0.42)
+	DefaultAirFriction          = float32(0.91)
+	DefaultBlockFriction        = float32(0.6)
+	NormalGravityMultiplier     = float32(0.98)
+	LevitationGravityMultiplier = float32(0.05)
+	NormalGravity               = float32(0.08)
+	SlowFallingGravity          = float32(0.01)
+	StepHeight                  = float32(0.6)
+	SlideOffsetMultiplier       = float32(0.4)
+	SlimeBounceMultiplier       = float32(-1)
+	BedBounceMultiplier         = float32(-0.66)
 	// This can be validated in Mob::ascendLadder()
 	ClimbSpeed = float32(0.2)
 
