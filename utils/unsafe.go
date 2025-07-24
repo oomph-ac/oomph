13bd511857d3cdc91207ce0ca136e4d9c5869fcd
ethaniccc <ethaniccc@oomph.ac>
2025-07-24 17:00:33 -0400

    Fix Oomph allowing wall-hit for mobile players (#83)
    Resolves #81

--- utils/unsafe.go
+++ utils/unsafe.go
@@ -8,6 +8,9 @@ import (
 	"time"
 	"unsafe"
 
+	"github.com/ethaniccc/float32-cube/cube"
+	"github.com/ethaniccc/float32-cube/cube/trace"
+	"github.com/go-gl/mathgl/mgl32"
 	"github.com/sandertv/go-raknet"
 	"github.com/sandertv/gophertunnel/minecraft"
 )
@@ -66,4 +69,20 @@ func BlockAddress(l *raknet.Listener, addr net.IP, duration time.Duration) {
 	defer sec.mu.Unlock()
 	sec.blockCount.Add(1)
 	sec.blocks[[16]byte(addr.To16())] = time.Now().Add(duration)
+}
+
+func ModifyBBoxResult(hitResult *trace.BBoxResult, bb cube.BBox, pos mgl32.Vec3, face cube.Face) {
+	type bboxResultWrapper struct {
+		bbox cube.BBox
+		pos  mgl32.Vec3
+		face cube.Face
+	}
+
+	if hitResult == nil {
+		return
+	}
+	hitResultWrapper := (*bboxResultWrapper)(unsafe.Pointer(hitResult))
+	hitResultWrapper.bbox = bb
+	hitResultWrapper.pos = pos
+	hitResultWrapper.face = face
 }
