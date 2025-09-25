6ebaddc2a351a04164ec1e98658e7d08442cdec7
ethaniccc <ethaniccc@oomph.ac>
2025-09-25 16:09:35 -0400

    punishment_event: add player identifier field

--- cloud/packet/punishment_event.go
+++ cloud/packet/punishment_event.go
@@ -3,6 +3,7 @@ package packet
 import "github.com/sandertv/gophertunnel/minecraft/protocol"
 
 type PunishmentEvent struct {
+	Identifier   string // 1-5 bytes + len(Identifier) bytes
 	PunishmentID string // 1-5 bytes + len(PunishmentID) bytes
 	EffectiveAt  int64  // 8 bytes
 }
@@ -12,6 +13,7 @@ func (*PunishmentEvent) ID() uint32 {
 }
 
 func (pk *PunishmentEvent) Marshal(io protocol.IO, cloudProto uint32) {
+	io.String(&pk.Identifier)
 	io.String(&pk.PunishmentID)
 	io.Int64(&pk.EffectiveAt)
 }
