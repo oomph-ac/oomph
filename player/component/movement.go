a6f1f16867cb97e409ed6f340135bd0bfa7a2568
ethaniccc <ethaniccc@oomph.ac>
2025-11-17 12:16:35 -0500

    go back to using -0.001 for bounding boxes

--- player/component/movement.go
+++ player/component/movement.go
@@ -474,7 +474,7 @@ func (mc *AuthoritativeMovementComponent) BoundingBox() cube.BBox {
 		mc.pos[0]+width,
 		mc.pos[1]+mc.size[1]+yOffset,
 		mc.pos[2]+width,
-	).GrowVec3(mgl32.Vec3{-1e-4, 0, -1e-4})
+	).GrowVec3(mgl32.Vec3{-1e-3, 0, -1e-3})
 }
 
 // ClientBoundingBox returns the bounding box of the movement component translated to the client's position.
