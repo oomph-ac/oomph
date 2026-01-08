73c384db250a9b69fa85d0d77761999f121ef349
ethaniccc <ethaniccc@oomph.ac>
2026-01-08 11:47:51 -0500

    attempt to fix possible weird client edge case for swing animation

--- player/detection/killaura_a.go
+++ player/detection/killaura_a.go
@@ -9,6 +9,8 @@ import (
 type KillauraA struct {
 	mPlayer  *player.Player
 	metadata *player.DetectionMetadata
+
+	checkDeferredSwing bool
 }
 
 func New_KillauraA(p *player.Player) *KillauraA {
@@ -44,20 +46,30 @@ func (d *KillauraA) Metadata() *player.DetectionMetadata {
 }
 
 func (d *KillauraA) Detect(pk packet.Packet) {
-	tpk, ok := pk.(*packet.InventoryTransaction)
-	if !ok {
-		return
-	}
+	switch pk := pk.(type) {
+	case *packet.InventoryTransaction:
+		if data, ok := pk.TransactionData.(*protocol.UseItemOnEntityTransactionData); ok && data.ActionType == protocol.UseItemOnEntityActionAttack {
+			tickDiff := int64(d.mPlayer.SimulationFrame) - d.mPlayer.Combat().LastSwing()
+			var maxTickDiff int64 = 10
+			if miningFatigue, ok := d.mPlayer.Effects().Get(packet.EffectMiningFatigue); ok {
+				maxTickDiff += int64(miningFatigue.Amplifier)
+			}
+			if tickDiff > maxTickDiff {
+				d.checkDeferredSwing = true
+			}
+		}
+	case *packet.PlayerAuthInput:
+		if !d.checkDeferredSwing {
+			return
+		}
 
-	lastSwung := d.mPlayer.Combat().LastSwing()
-	if data, ok := tpk.TransactionData.(*protocol.UseItemOnEntityTransactionData); ok && data.ActionType == protocol.UseItemOnEntityActionAttack {
+		lastSwung := d.mPlayer.Combat().LastSwing()
 		currentTick := d.mPlayer.SimulationFrame
 		tickDiff := int64(currentTick) - lastSwung
 		var maxTickDiff int64 = 10
 		if miningFatigue, ok := d.mPlayer.Effects().Get(packet.EffectMiningFatigue); ok {
 			maxTickDiff += int64(miningFatigue.Amplifier)
 		}
-
 		if tickDiff > maxTickDiff {
 			d.mPlayer.FailDetection(
 				d,
@@ -67,4 +79,5 @@ func (d *KillauraA) Detect(pk packet.Packet) {
 			)
 		}
 	}
+
 }
